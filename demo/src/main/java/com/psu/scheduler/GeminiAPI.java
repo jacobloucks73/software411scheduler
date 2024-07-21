4d1148b160cf6c9d96db4af13d7ea443d83037d4
Jacob Loucks <jfl6196@psu.edu>
2024-07-21 03:07:10 -0400

    RECONFIGURED PROJECT STRUCTURE, DATE:07/12/2024 SIGNED: JACOB
    ADDED:
    AUTHSERVICE.JAVA
    WEBCONTROLLER.JAVA
    HTTPREQUEST.JAVA
    DATABASE.JAVA
    EMAILSERVICE.JAVA
    GEMINIAPI.JAVA
    MAIN.JAVA
    SCHEDULER.JAVA
    PROJECT.JAVA
    NOTIFICATION.JAVA
    
    FIXED:
    POM.XML
    
    ADDED COMPLETED CODE TO:
    GEMINIAPI.JAVA

--- demo/src/main/java/com/psu/scheduler/GeminiAPI.java
+++ demo/src/main/java/com/psu/scheduler/GeminiAPI.java
@@ -1,62 +1,7 @@
 package com.psu.scheduler;
-/*/
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.aiplatform.v1.PredictionServiceClient;
-import com.google.cloud.aiplatform.v1.PredictRequest;
-import com.google.cloud.aiplatform.v1.PredictResponse;
-import com.google.cloud.aiplatform.v1.EndpointName;
-import com.google.protobuf.Value;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Arrays;
-
-public class GeminiAPI {
-    public static void main(String[] args) {
-        try {
-            // Use the direct file path to the credentials JSON
-            String credentialsPath = "C:\\Users\\FBIVAN2\\Downloads\\SWENG411Project\\demo\\src\\main\\resources\\taskscheduler-429105-fcf0e597b021.json";
-            System.out.println("Using credentials file: " + credentialsPath);
-
-            // Initialize credentials
-            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
-            System.out.println("Credentials successfully initialized.");
-
-            // Initialize the Vertex AI client
-            PredictionServiceClient predictionServiceClient = PredictionServiceClient.create();
-            System.out.println("Prediction service client successfully initialized.");
-
-            // Set the endpoint name and request
-            String projectId = "taskscheduler-429105";
-            String location = "us-cent ral1";
-            String endpointId = "https://us-east1-aiplatform.googleapis.com";
-            EndpointName endpointName = EndpointName.of(projectId, location, endpointId);
-
-            // Create an instance
-            Value instanceValue = Value.newBuilder().setStringValue("Hello, Gemini!").build();
-
-            // Create the request
-            PredictRequest predictRequest = PredictRequest.newBuilder()
-                    .setEndpoint(endpointName.toString())
-                    .addAllInstances(Arrays.asList(instanceValue))
-                    .build();
-
-            // Make the prediction
-            PredictResponse response = predictionServiceClient.predict(predictRequest);
-            System.out.println("Prediction response: " + response);
-
-            // Close the client
-            predictionServiceClient.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-/*/
 
 import com.google.cloud.vertexai.VertexAI;
-import com.google.cloud.vertexai.api.Blob;
-import com.google.cloud.vertexai.api.Content;
 import com.google.cloud.vertexai.api.GenerateContentResponse;
 import com.google.cloud.vertexai.api.GenerationConfig;
 import com.google.cloud.vertexai.api.HarmCategory;
@@ -64,13 +9,8 @@ import com.google.cloud.vertexai.api.Part;
 import com.google.cloud.vertexai.api.SafetySetting;
 import com.google.cloud.vertexai.generativeai.ContentMaker;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
-import com.google.cloud.vertexai.generativeai.PartMaker;
 import com.google.cloud.vertexai.generativeai.ResponseStream;
-import com.google.protobuf.ByteString;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -111,22 +51,27 @@ public class GeminiAPI {
 
             // Create parts for the prompt input
             Part part = Part.newBuilder()
-                    .setText("This is a sample prompt text.")
+                    .setText("please give me multiple different formatting choices so i can see what they all are, please do: bold,italics,underline, and indentation. please label them all as well before you've done it" )
                     .build();
 
             var content = ContentMaker.fromMultiModalData(part);
             ResponseStream<GenerateContentResponse> responseStream = model.generateContentStream(content);
-
+            StringBuilder fullText = new StringBuilder();
             // Do something with the response
             responseStream.stream().forEach(response -> {
                 response.getCandidatesList().forEach(candidate -> {
-                    StringBuilder fullText = new StringBuilder();
                     candidate.getContent().getPartsList().forEach(partContent -> {
                         fullText.append(partContent.getText());
                     });
-                    System.out.println("Generated Text: " + fullText.toString());
+
                 });
+
             });
+            //Start of postprocessing on the text
+
+
+            System.out.println("Gen4" +
+                    ".erated Text: " + fullText.toString());
         }
     }
 }
